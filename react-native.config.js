62b55f6601dbf60e0a17b7f813fb86cf93fa6bec
延飞 李 <liyanfei_2011@163.com>
2023-06-07 18:03:46 +0800

    no message

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,10 @@ module.exports = {
   dependency: {
     platforms: {
       ios: { project: "lib/ios/react-native-baidumap-sdk-fg.podspec" },
-      android: { sourceDir: "lib/android" }
+      android: { sourceDir: "lib/android",
+        packageImportPath:
+            "import cn.qiuxiang.react.baidumap.BaiduMapPackage;",
+        packageInstance: "new BaiduMapPackage()",}
     }
   },
   dependencies: {
@@ -10,11 +13,6 @@ module.exports = {
       root: __dirname,
       platforms: {
         ios: { podspecPath: __dirname + "/lib/ios/react-native-baidumap-sdk-fg.podspec" },
-        android: {
-          sourceDir: __dirname + "/lib/android",
-          packageImportPath: "import cn.qiuxiang.react.baidumap.BaiduMapPackage;",
-          packageInstance: "new BaiduMapPackage()"
-        }
       }
     }
   }
