321122b33dfe701ae87c51eb5d034c8369995733
延飞 李 <liyanfei_2011@163.com>
2023-06-13 17:52:07 +0800

    no message

--- lib/android/src/main/java/cn/qiuxiang/react/baidumap/modules/BaiduMapGeocodeModule.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/baidumap/modules/BaiduMapGeocodeModule.kt
@@ -10,33 +10,34 @@ import org.json.JSONArray
 
 
 
-//这个jar包中没有这个，这个功能单独拿出来实现的
+//百度sdk 用kt写总是崩溃，拿出来单独写
 @Suppress("unused")
 class BaiduMapGeocodeModule(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
     private var promise: Promise? = null
-    private val geoCoder by lazy {
-        val geoCoder = PoiSearch.newInstance();
-        var listener = geoCoder.setOnGetPoiSearchResultListener(
-            object : OnGetPoiSearchResultListener {
-                override fun onGetPoiResult(p0: PoiResult?) {
-                    TODO("Not yet implemented")
-                }
 
-                override fun onGetPoiDetailResult(p0: PoiDetailResult?) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onGetPoiDetailResult(p0: PoiDetailSearchResult?) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onGetPoiIndoorResult(p0: PoiIndoorResult?) {
-                    TODO("Not yet implemented")
-                }
-
-            } )
-        geoCoder
-    }
+//    private val geoCoder by lazy {
+//        val geoCoder = PoiSearch.newInstance();
+//        var listener = geoCoder.setOnGetPoiSearchResultListener(
+//            object : OnGetPoiSearchResultListener {
+//                override fun onGetPoiResult(p0: PoiResult?) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onGetPoiDetailResult(p0: PoiDetailResult?) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onGetPoiDetailResult(p0: PoiDetailSearchResult?) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onGetPoiIndoorResult(p0: PoiIndoorResult?) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//            } )
+//        geoCoder
+//    }
 
     override fun getName(): String {
         return "BaiduMapGeocode"
@@ -46,53 +47,53 @@ class BaiduMapGeocodeModule(context: ReactApplicationContext) : ReactContextBase
         return true
     }
 
-    @ReactMethod
-    fun search( obj:ReadableArray, promise: Promise) {
-        if (this.promise == null) {
-            this.promise = promise
-            /**
-             *  PoiCiySearchOption 设置检索属性
-             *  city 检索城市
-             *  keyword 检索内容关键字
-             *  pageNum 分页页码
-             */
-            /**
-             * PoiCiySearchOption 设置检索属性
-             * city 检索城市
-             * keyword 检索内容关键字
-             * pageNum 分页页码
-             */
-
-              val array: WritableArray? = Arguments.createArray()
-               var arr: JSONArray? = null
-                arr = JSONArray(obj.toString())
-                val jsonObject = arr!!.optJSONObject(0)
-//        JSONObject jsonObject = new JSONObject(String.valueOf(object));
-                //        JSONObject jsonObject = new JSONObject(String.valueOf(object));
-                val types = jsonObject.getString("types")
-                val keywords = jsonObject.getString("keywords")
-                val city = jsonObject.getString("city")
-                geoCoder.searchInCity(
-                    PoiCitySearchOption()
-                        .city(city) //必填
-                        .keyword(keywords).tag(types).cityLimit(false) //必填
-                        .pageNum(0)
-                )
-
-//            geoCoder.geocode(GeoCodeOption().address(address).city(city))
-        } else {
-            promise.reject("", "This callback type only permits a single invocation from native code")
-        }
-    }
-
-    @ReactMethod
-    fun reverse(coordinate: ReadableMap, promise: Promise) {
-        if (this.promise == null) {
-            this.promise = promise
-            geoCoder.destroy();
-//            geoCoder.reverseGeoCode(ReverseGeoCodeOption().location(coordinate.toLatLng()))
-        } else {
-            promise.reject("", "This callback type only permits a single invocation from native code")
-        }
-    }
+//    @ReactMethod
+//    fun search( obj:ReadableArray, promise: Promise) {
+//        if (this.promise == null) {
+//            this.promise = promise
+//            /**
+//             *  PoiCiySearchOption 设置检索属性
+//             *  city 检索城市
+//             *  keyword 检索内容关键字
+//             *  pageNum 分页页码
+//             */
+//            /**
+//             * PoiCiySearchOption 设置检索属性
+//             * city 检索城市
+//             * keyword 检索内容关键字
+//             * pageNum 分页页码
+//             */
+//
+//              val array: WritableArray? = Arguments.createArray()
+//               var arr: JSONArray? = null
+//                arr = JSONArray(obj.toString())
+//                val jsonObject = arr!!.optJSONObject(0)
+////        JSONObject jsonObject = new JSONObject(String.valueOf(object));
+//                //        JSONObject jsonObject = new JSONObject(String.valueOf(object));
+//                val types = jsonObject.getString("types")
+//                val keywords = jsonObject.getString("keywords")
+//                val city = jsonObject.getString("city")
+//                geoCoder.searchInCity(
+//                    PoiCitySearchOption()
+//                        .city(city) //必填
+//                        .keyword(keywords).tag(types).cityLimit(false) //必填
+//                        .pageNum(0)
+//                )
+//
+////            geoCoder.geocode(GeoCodeOption().address(address).city(city))
+//        } else {
+//            promise.reject("", "This callback type only permits a single invocation from native code")
+//        }
+//    }
+//
+//    @ReactMethod
+//    fun reverse(coordinate: ReadableMap, promise: Promise) {
+//        if (this.promise == null) {
+//            this.promise = promise
+//            geoCoder.destroy();
+////            geoCoder.reverseGeoCode(ReverseGeoCodeOption().location(coordinate.toLatLng()))
+//        } else {
+//            promise.reject("", "This callback type only permits a single invocation from native code")
+//        }
+//    }
 }
